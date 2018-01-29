6b493c5be4d24a0d0c246ae9d8fbfea9dd0e07f2
heanuea <g00318763@gmit.com>
2018-01-29 18:52:31 +0000

    cant print list and end recursion

--- Task2/Task2.rkt
+++ Task2/Task2.rkt
@@ -17,3 +17,16 @@
             (collatz-list (/ n 2) (+ counter 1))
             
             (collatz-list (+ (* n 3) 1) (+ counter 1))))))
+
+
+
+;-----------------------------------------------
+
+            (define (collatz n)
+  (cond ((= n 1) 1)
+        ((> n 1) (collatz_h n))))
+
+(define (collatz-list n)
+   (if (even? n)
+      (collatz (/ n 2))
+         (collatz-list (+ (* 3 n) 1))))
