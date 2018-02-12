b36ebb7947edb6fd08f026955187758708262822
heanuea <g00318763@gmit.com>
2018-02-12 19:11:07 +0000

    got them printing but not a list

--- Task2/Task2.rkt
+++ Task2/Task2.rkt
@@ -1,35 +1,15 @@
 #lang racket
 
 
-
-
-
-
-(define (collatz '())
-
-  ;function 
-  (define (collatz-list n counter)
-
-  ;condition 
-    (if (<=  n 1)
-        1
-        (if (even? n)
-            (collatz-list (/ n 2) (+ counter 1))
-            
-            (collatz-list (+ (* n 3) 1) (+ counter 1))))))
-
-
-(odd 4)
-(even 4)
-(collatz-list 10)
-
-;-----------------------------------------------
-
-            (define (collatz n)
-  (cond ((= n 1) 1)
-        ((> n 1) (collatz_h n))))
-
 (define (collatz-list n)
-   (if (even? n)
-      (collatz (/ n 2))
-         (collatz-list (+ (* 3 n) 1))))
+  (if (even? n)
+      (/ n 2)
+      (+ (* 3 n) 1)))
+ 
+(define (collatz-range m)
+  (for-each (lambda (n) (collatz-list n))
+            (stream->list (in-range 5 m))))
+ 
+(collatz-list 5)
+(collatz-list 9)
+(collatz-list 2) 
