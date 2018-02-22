d5181b3c1b388410cb0225d1df81a8d7f13d4337
heanuea <g00318763@gmit.com>
2018-02-22 17:16:53 +0000

    Added Con and changed around added recursion

--- Task2/Task2.rkt
+++ Task2/Task2.rkt
@@ -1,15 +1,25 @@
+
 #lang racket
 
 
-(define (collatz-list n)
-  (if (even? n)
-      (/ n 2)
-      (+ (* 3 n) 1)))
- 
-(define (collatz-range m)
-  (for-each (lambda (n) (collatz-list n))
-            (stream->list (in-range 5 m))))
- 
+
+(display "Problem 2 \n")
+(define (collatz-list n);define function
+  (if (= n 1);check if n = 1
+      
+      (cons n true)
+      
+   (cons n(if (integer? (/ n 2))
+
+          (collatz-list (/ n 2));recursion 
+          (collatz-list (+ 1 (* n 3)))))
+      )
+  )
+
+ ;Print out list for testing
+(display "\nResults: ")
 (collatz-list 5)
+(display "\nResults: ")
 (collatz-list 9)
+(display "\nResults: ")
 (collatz-list 2) 
