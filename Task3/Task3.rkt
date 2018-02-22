61ce48f83604902e9db33ce8192c2d0d5c605941
heanuea <g00318763@gmit.com>
2018-02-22 19:40:06 +0000

    Added task 3

---
+++ Task3/Task3.rkt
@@ -0,0 +1,16 @@
+#lang racket 
+
+;create a function for left cycle called lcycle 
+(define lcycle 1)
+
+;create 2nd fuction for right cycle called rcycle 
+(define rcycle 1)
+
+
+
+
+
+; Print out results 
+(lcycle(list 1 2 3 4 5))
+
+(rcycle(list 1 2 3 4 5)) 
