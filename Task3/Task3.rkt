fe4843a24f839992bf329d54e5dec70f7f418788
heanuea <g00318763@gmit.com>
2018-03-05 10:19:37 +0000

    added solution

--- Task3/Task3.rkt
+++ Task3/Task3.rkt
@@ -1,16 +1,29 @@
 #lang racket 
 
 ;create a function for left cycle called lcycle 
-(define lcycle 1)
+(define (lcycle l)
+     (if (null? l) ;  list = null
+      '() ; output null
+      (append (cdr l) (list (car l))))) ;  append it to list
 
 ;create 2nd fuction for right cycle called rcycle 
-(define rcycle 1)
+(define (rcycle l)
+      (if (null? l) ; if list = null then..
+      '() ; output null
+      (append (cons (last l)(remove-last l))))) 
 
 
 
+; remove-last from list
+(define (remove-last l)
+  (if (null? (cdr l))
+        '() ;output null
+        (cons (car l) (remove-last (cdr l))))) ; newly allocated list 
+  
 
 
 ; Print out results 
 (lcycle(list 1 2 3 4 5))
 
 (rcycle(list 1 2 3 4 5)) 
+
