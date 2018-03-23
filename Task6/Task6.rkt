ee49aac15ca23ea7747c9d2e8a84f0e6d26b723c
heanuea <g00318763@gmit.com>
2018-03-23 13:12:37 +0000

    added functions looking how to solve now

---
+++ Task6/Task6.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(define n 0)
+; define function = 2 lists 
+(define (hamming-distance l m) 
+
+
+;pring out the 
+(hamming-distance (list 1 1 1 1 0 0 0 0) (list  1 0 1 0 1 1 1 0))
+
+
