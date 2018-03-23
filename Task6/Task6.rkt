17907e0cbdf5d2493be63f2656754bbda9d90aca
heanuea <g00318763@gmit.com>
2018-03-23 13:26:59 +0000

    6 done

--- Task6/Task6.rkt
+++ Task6/Task6.rkt
@@ -2,10 +2,19 @@
 
 (define n 0)
 ; define function = 2 lists 
-(define (hamming-distance l m) 
+(define (hamming-distance l t)
+; if statement to see if not equal to 1  
+  (if (not (pair? l))
+; returns the difference 
+      n 
+      ; if the same keep calling 
+      (if (= (car l) (car t)) (hamming-distance (cdr l) (cdr t)) 
+      ;else we add the distance by 1 
+      (+ 1 (hamming-distance (cdr l) (cdr t)))))) 
 
 
-;pring out the 
+;pring out the distance 
+(displayln '(Here is the distance between 2 list))
 (hamming-distance (list 1 1 1 1 0 0 0 0) (list  1 0 1 0 1 1 1 0))
 
 
